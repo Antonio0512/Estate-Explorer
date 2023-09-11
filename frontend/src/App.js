d2faecd92a54afd95e4c3ff278330f3f125eb3b5
Antonio0512 <antonioboyanov9@gmail.com>
2023-09-11 13:08:38 +0300

    Added initial configurations on the React side

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {Home} from './containers/Home';
+import {About} from './containers/About';
+import {Contact} from './containers/Contact';
+import {Listings} from './containers/Listings';
+import {ListingDetails} from './containers/ListingDetails';
+import {SignUp} from './containers/SignUp';
+import {SignIn} from './containers/SignIn';
+import {NotFound} from "./components/NotFound";
+import {Layout} from './hocs/Layout';
+
+import "./sass/main.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/contact' element={<Contact/>}/>
+                <Route path='/listings' element={<Listings/>}/>
+                <Route path='/listings/:id' element={<ListingDetails/>}/>
+                <Route path='/signup' element={<SignUp/>}/>
+                <Route path='/signin' element={<SignIn/>}/>
+
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const NotFound = () => {
+    return (
+        <div className="notfound">
+            <h1 className="notfound__heading">404 Not Found</h1>
+            <p className="notfound__paragraph">The url you requested does not exist on our website.</p>
+        </div>
+    );
+}
